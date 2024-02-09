be85933fe0bdbc25f0cabb64fb08273025f954af
pabbasaikiran3 <pabbasaikiran3@gmail.com>
2024-02-09 16:35:03 +0530

    commit "new modified"

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,10 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-0c7217cdde317cfec"
     instance_type = "t2.micro"
+    subnet_id = "subnet-0fa84f7b549aad4cd"
+    key_name = "Pabba123"
 }
