9b1bd2a8396bc09e0bd13b0f8087e6d8482b41e7
pabbasaikiran3 <pabbasaikiran3@gmail.com>
2024-02-13 19:02:16 +0530

    "new update"

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -6,5 +6,5 @@ module "ec2_instance" {
   source = "./modules/ec2_instance"
   ami_value = "ami-0c7217cdde317cfec"
   instance_type_value = "t2.medium"
-  subnet_id_value = "subnet-019ea91ed9b5252e7"
+  subnet_id_value = "subnet-0fa84f7b549aad4cd"
 }
